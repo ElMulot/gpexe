c1eacaa13555e2131e4dfbfacb952201f1acf008
ElMulot <rocodutibet@hotmail.com>
2022-09-17 23:53:31 +0200

    17-09-2022-11

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ Encore
 	// enables @babel/preset-env polyfills
 	.configureBabelPresetEnv((config) => {
 		config.useBuiltIns = 'usage';
-		config.corejs = 3;
+		config.corejs = '3.23';
 	})
 
 	// enables Sass/SCSS support
